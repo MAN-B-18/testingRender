c097abf5d25ecc875fda6d946a9f1189541fb17f
MAN-B-18 <siyabongajustin716@gmail.com>
2026-03-22 22:42:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-    document.getElementById("message").innerText = "🚀 You are learning real-world web development!";
-}
-
-function submitForm() {
-    let name = document.getElementById("name").value;
-    document.getElementById("response").innerText = "Thank you, " + name + "!";
-}
